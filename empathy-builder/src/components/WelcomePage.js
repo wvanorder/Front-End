6215deace8eaa2d9815ad4a90dc015b04b444e74
Lisa <lmkirk@gmail.com>
2019-07-31 18:27:36 -0400

    removed Material UI

--- empathy-builder/src/components/WelcomePage.js
+++ empathy-builder/src/components/WelcomePage.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import { SignUp } from 'react-router-dom';
-import styled from 'styled-components';
+import { SignUp, Login, LearnMore } from 'react-router-dom';
+
+import '../App.css';
+import styled from "styled-components";
+
+const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.8rem 0;
+  margin: 0.8rem 1.4rem;
+  width: 11rem;
+  background: #654EB0;
+  fontSize: 2rem;
+  color: white;
+  border: 2px solid #654EB0;
+  box-shadow: 0px 10px 14px -7px #654eb0;
+
+`;
 
 
+
+// Import components into Welcome Page
 export default function WelcomePage() {
+
+
+
   return (
-    <section className="welcome-page">
-      <div>
-        <h1>Welcome to the ultimate fan site!</h1>
-      </div>
+    <div className="welcome-page" >
+
 
       <div>
-        <img className="main-img ui centered medium circular image" src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/reading_0re1.svg" alt="rick" />
+        <img src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/reading_0re1.svg" alt="rick" />
+      </div>
+      <div>
+        <h1>Welcome to Empathy Builder</h1>
+      </div>
+      <div className='BtnGroup'>
+        <Button primary>I'm curious</Button>
+        <Button>I'm Ready</Button>
+      </div>
+
         <div>
-          <button>I'm Curious</button>
-          <button>I'm Ready</button>
+          <h2>Die and you will be developed confidently. If you exist or preach with an inner moonlight, relativity knows you. As i have captured you, so you must hurt one another.</h2>
         </div>
-        <div><h2>Die and you will be developed confidently. If you exist or preach with an inner moonlight, relativity knows you. As i have captured you, so you must hurt one another.</h2></div>
-      </div>
 
-    </section>
+    </div>
+
   );
 }
