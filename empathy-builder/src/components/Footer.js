8c76cd95c95800e3987fbf2fa821661f1c6c1a8b
Lisa <lmkirk@gmail.com>
2019-08-01 10:44:59 -0400

    Created Footer component. Added phantom div to LearnMore to add spacing for NavBar

--- empathy-builder/src/components/Footer.js
+++ empathy-builder/src/components/Footer.js
@@ -3,17 +3,23 @@ import React from 'react';
 import '../App.css';
 import styled from "styled-components";
 
-const Container = styled.div`
-    padding: 3rem;
-    background: #F9BBB1;
+
+const FooterDiv = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%; padding: 1rem;
+  background: #F9BBB1;
 `;
+
+
 
 export default function Footer() {
 
   return (
-    <Container>
+    <FooterDiv>
       <div>Copyright and cool stuff go here</div>
       <div>Disclaimer text here</div>
-    </Container>
+    </FooterDiv>
   );
 }
--- empathy-builder/src/components/LearnMore.js
+++ empathy-builder/src/components/LearnMore.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Footer from "./Footer";
+
+
+const phantom = {
+  display: 'block',
+  padding: '3rem',
+  height: '60px',
+  width: '100%',
+};
 
 export default function LearnMore() {
 
   return (
     <div>
+      <div style={phantom} />
      <h2>Resources</h2>
+      <Footer/>
     </div>
   )
 
